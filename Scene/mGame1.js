d4905461fa8a5d718dd747bdfb8f4c1fea826432
Nathand Dournaux <dournauxnathan@gmail.com>
2020-06-10 11:52:10 +0200

    Implémentation001

--- Scene/mGame1.js
+++ Scene/mGame1.js
@@ -11,12 +11,12 @@ class mGame1 extends Phaser.Scene {
     }
 
 	preload() {
-        this.load.image("timebar", "Proto/timer.png");
-        this.load.image("bar", "Proto/bar.png");
-        this.load.image("barindicator", "Proto/blue.png");
 	}
 
 	create() {
+		this.add.image(0, 0, 'background1').setOrigin(0,0);
+		this.add.sprite(1150, 125, "bar").setOrigin(0,0).setScale();
+
 		//Timer
 			let gameOptions = { initialTime: 600 }
 			this.timeLeft = gameOptions.initialTime;
@@ -40,77 +40,65 @@ class mGame1 extends Phaser.Scene {
 	        });
 	        this.gameTimer.paused = false;
 
-	    this.bar = this.add.sprite(30, 150, "bar").setOrigin(0,0).setScale();
-	    this.barindicatorX = 30;
-	    this.barindicatorY = 600;
-
-	    this.input.on('pointerdown', function (pointer, barindicator)
-	    {	
-	    	if(this.gameTimer.paused == false)
-		    {
-		    	this.barindicatorY -= 10;
-		    }
-	    }, this);
-
 	    //Exemple de constructors : Boutons
-		    /*
-		    function PausedButtons(sprite, x, y, here,gameTimer)
-		    {
-		    	this._button = here.add.sprite(x,y,sprite).setInteractive();
+			/*
+			function PausedButtons(sprite, x, y, here,gameTimer)
+			{
+				this._button = here.add.sprite(x,y,sprite).setInteractive();
 
-		    	this._button.on('pointerover', () => {
-		    		this._button.setTint(0xffffff)
-		    	})
+				this._button.on('pointerover', () => {
+					this._button.setTint(0xffffff)
+				})
 
-		    	this._button.on('pointerout', () => {
-		    		this._sbuttonclearTint();
-		    	})
+				this._button.on('pointerout', () => {
+					this._sbuttonclearTint();
+				})
 
-		    	this._button.on('pointerdown', () => {
+				this._button.on('pointerdown', () => {
 			    	this.gameTimer.paused = true;
 			    });
-		    }
+			}
 
-		    function ResumeButtons(sprite, x, y, here)
-		    {
-		    	this._button = here.add.sprite(x,y,sprite).setInteractive();
+			function ResumeButtons(sprite, x, y, here)
+			{
+				this._button = here.add.sprite(x,y,sprite).setInteractive();
 
-		    	this._button.on('pointerover', () => {
-		    		this._button.setTint(0xffffff)
-		    	})
+				this._button.on('pointerover', () => {
+					this._button.setTint(0xffffff)
+				})
 
-		    	this._button.on('pointerout', () => {
-		    		this._button.clearTint();
-		    	})
+				this._button.on('pointerout', () => {
+					this._button.clearTint();
+				})
 
-		    	this._button.on('pointerdown', () => {
+				this._button.on('pointerdown', () => {
 			    	this.gameTimer.paused = false;
 			    });
-		    }
+			}
 
-		    function ExitButtons(sprite, x, y, here)
-		    {
-		    	this._button = here.add.sprite(x,y,sprite).setInteractive();
+			function ExitButtons(sprite, x, y, here)
+			{
+				this._button = here.add.sprite(x,y,sprite).setInteractive();
 
-		    	this._button.on('pointerover', () => {
-		    		this._button.setTint(0xffffff)
-		    	})
+				this._button.on('pointerover', () => {
+					this._button.setTint(0xffffff)
+				})
 
-		    	this._button.on('pointerout', () => {
-		    		this._button.clearTint();
-		    	})
+				this._button.on('pointerout', () => {
+					this._button.clearTint();
+				})
 
-		    	this._button.on('pointerdown', () => {
+				this._button.on('pointerdown', () => {
 			    	this.gameTimer.paused = pause;
 			    });
-		    }
+			}
 
 		    this.pauseButton = new PausedButtons("pauseButton", 1230,80, this);
 		    this.resumeButton = new ResumeButtons("resumeButton", 540,640, this);
 		    this.quitButton = new ExitButtons("quitButton", 740,640, this);
 			*/
-		
-		//Mettre en pause le jeu - 2 issues : Continue(retour au jeu) & Quitter(retour au menu principal)
+	    
+	    //Mettre en pause le jeu - 2 issues : Continue(retour au jeu) & Quitter(retour au menu principal)
 		    this.pauseButton = this.add.sprite(1230,80, 'pausedButton').setInteractive();
 		    this.resumeButton = this.add.sprite(440,360, 'resumeButton').setInteractive().setVisible(false);
 		    this.quitButton = this.add.sprite(740,360, 'quitButton').setInteractive().setVisible(false);
@@ -133,26 +121,37 @@ class mGame1 extends Phaser.Scene {
 		    		this.cameras.main.fade(0x000000, 1);
 		    		this.scene.start("main");
 		    	});
-		    });  
+		    }); 
+
+		//UI
+	    	this.barindicator = this.add.sprite(1203,600, "barindicator").setOrigin(0,0).setAngle(105).setScale(0.75);
+
+	    //Interaction with the player - Inputs 
+		    this.input.on('pointerdown', function (pointer, barindicator)
+		    {	
+		    	if(this.gameTimer.paused == false)
+			    {
+			    	this.barindicator.y -= 10;
+			    	console.log(this.barindicator.y);
+			    }
+		    }, this);
 	}
 
 	update() {
-		this.barindicator = this.add.sprite(this.barindicatorX,this.barindicatorY, "barindicator").setOrigin(0,0);
+		//Win condition
+			if(this.barindicator.y <= 100 && this.timeLeft != 0)
+			{
+				this.level++;
+				this.gameTimer.paused = true;
+				this.score++;
+				this.scene.start("issue", {nVie: this.nVie, score: this.score, level: this.level});
+			}
 
-		if(this.barindicatorY <= 150 && this.timeLeft != 0)
-		{
-			this.level++;
-			this.gameTimer.paused = true;
-			this.score++;
-			this.scene.start("issue", {nVie: this.nVie, score: this.score, level: this.level});
-		}
-
-		if(this.timeLeft == 0){
-			this.level++;
-        	this.nVie--;
-            this.scene.start("issue", {nVie: this.nVie, score: this.score, level: this.level});
-
-        }
-		
+		//Fail Condition
+			if(this.timeLeft == 0){
+				this.level++;
+	        	this.nVie--;
+	            this.scene.start("issue", {nVie: this.nVie, score: this.score, level: this.level});
+	        }
 	}
 }
